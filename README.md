b906ef60aeb38745a5deea27142f9d9a9b83c6cd
Momoko Toyota <toyopet-momoko@hotmail.co.jp>
2021-05-01 23:27:22 +0900

    login & signup form added

--- README.md
+++ README.md
@@ -7,3 +7,7 @@
 2. Install new cli `npm install -g @vue/cli`
 3. Create New Project: `vue create live-chat-firebase`
 4. Change directory: `cd live-chat-firebase`    
+5. add a Welcome page & css
+6. add firebase
+   create a firebase directory
+7. Add signup and login page with setup method
--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -1,14 +1,42 @@
 <template>
   <div class="container welcome">
-    <p>
-      Welcome
-    </p>
+    <p>Welcome</p>
+    <sign-up-form />
+    <login-form />
   </div>
 </template>
+
+<script>
+import SignUpForm from '../components/SignUpForm'
+import LoginForm from '../components/LoginForm'
+
+export default {
+  components: { SignUpForm, LoginForm }
+}
+</script>
 
 <style>
   .welcome {
     text-align: center;
     padding: 20px 0;
+  }
+
+  /* form styles */
+  .welcome form {
+    width: 300px;
+    margin: 20px auto;
+  }
+  .welcome label {
+    display: block;
+    margin: 20px 0 10px;
+  }
+  .welcome input {
+    width: 100%;
+    padding: 10px;
+    border-radius: 20px;
+    border: 1px solid #eee;
+    outline: none;
+    color: #999;
+    margin: 10px auto;
   }
 </style>
