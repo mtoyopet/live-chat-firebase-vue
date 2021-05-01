80168d9fa7e861cdf959b332438b8509338a4c94
Momoko Toyota <toyopet-momoko@hotmail.co.jp>
2021-05-01 22:55:52 +0900

    init

---
+++ src/views/Welcome.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="container welcome">
+    <p>
+      Welcome
+    </p>
+  </div>
+</template>
+
+<style>
+  .welcome {
+    text-align: center;
+    padding: 20px 0;
+  }
+</style>
